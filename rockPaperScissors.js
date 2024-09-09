a0ed48e1fa5d6e3a9093ed92753266bd2a89401c
standardstd <mampuyareagan@gmail.com>
2024-09-09 16:15:28 -0400

    feat:Make human (unvalidated) choice

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,7 +1,25 @@
+// Computer's choice
 function getComputerChoice() {
     let choice = [ "rock", "paper", "scissors"]
     return choice[Math.floor(Math.random()*3)]; 
 }
 
-x = getComputerChoice();
-console.log(x);
+// Human choice
+function getHumanChoice() {
+    let choice = [ "rock", "paper", "scissors"]
+    let text = `What is your choice:
+                    0: Rock
+                    1: Paper
+                    2: Scissors
+    Please make a choice between 0 and 2:  `
+
+    let humanEntry = parseInt(prompt(text));
+    return choice[humanEntry]; 
+}
+
+
+
+let computerChoice = getComputerChoice();
+console.log(computerChoice);
+let humanChoice = getHumanChoice();
+console.log(humanChoice); 
