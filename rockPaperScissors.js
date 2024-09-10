6cc6b2fe9aeb298be3f0f5df947b16f8861b5d75
standardstd <mampuyareagan@gmail.com>
2024-09-09 21:12:49 -0400

    feat: Create a function which determines the winner

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,14 +1,14 @@
 let humanScore, computerScore = 0;
-let choice = [ "rock", "paper", "scissors"]
+let choice = ["paper", "scissors", "rock"]  //In natural order of winning. Exception are treated separetely
 
 // Computer's choice
 function getComputerChoice() {
-    return choice[Math.floor(Math.random()*3)]; 
+    return choice[Math.floor(Math.random() * 3)];
 }
 
 // Human choice
 function getHumanChoice() {
-    let choice = [ "rock", "paper", "scissors"]
+    // let choice = ["paper", "scissors", "rock"]
     let humanEntry;
     let text = `What is your choice:
                     * Rock
@@ -16,25 +16,63 @@ function getHumanChoice() {
                     * Scissors
     Please make a choice only between those three elements:  `
 
-    do
-    {
+    do {
         humanEntry = prompt(text).toLowerCase();
     }
-    while(choice.findIndex((data) => (data == humanEntry)) === -1);
-     
-    return humanEntry; 
+    while (choice.findIndex((data) => (data == humanEntry)) === -1);
+
+
+    return humanEntry;
 }
 
 
 
 function playRound(humanChoice, computerChoice) {
-    if(humanChoice > computerChoice) return;
+    
+    //Tie
+    if ((choice.indexOf(humanChoice)) === (choice.indexOf(computerChoice))) {
+        printRoundWinner("", humanChoice, computerChoice, 1);
+        return;
+    }
+    //Tests if human win
+    if ((humanChoice === "paper") && (computerChoice === "rock")) {
+        humanScore++;
+        printRoundWinner("win", humanChoice, computerChoice, 0);
+        return;
+    }
+
+    if ((computerChoice === "paper") && (humanChoice === "rock")) {
+        computerScore++;
+        printRoundWinner("loose", computerChoice, humanChoice, 0);
+        return;
+    }
+
+    if ((choice.indexOf(humanChoice)) > (choice.indexOf(computerChoice))) {
+        humanScore++;
+        printRoundWinner("win", humanChoice, computerChoice, 0);
+        return;
+    }
+    
+    //Computer win
+    printRoundWinner("loose", computerChoice, humanChoice, 0);
+    computerScore++;
+    return;
+}
+
+function printRoundWinner (result, winner, looser, tie) {
+    if(tie === 1) {
+        console.log("No one win");
+        
+    }else{
+        console.log(`You ${result}, ${winner} beats ${looser}  `);
+    }
 }
 
 
 
-
-const computerChoice = getComputerChoice(); 
-console.log(computerChoice);
+const computerChoice = getComputerChoice();
+console.log("Computer: " + computerChoice);
 const humanChoice = getHumanChoice();
-console.log(humanChoice);  
+console.log("human: " + humanChoice);  
+playRound(humanChoice, computerChoice); 
+
